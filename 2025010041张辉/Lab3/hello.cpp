a07b4343f774e70b6ac54c1bd7149db06919e1f4
Your Name <you@example.com>
2026-09-09 12:50:04 +0800

    完成Lab3作业

--- 2025010041张辉/Lab3/hello.cpp
+++ 2025010041张辉/Lab3/hello.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 bool getDoubled(int value, int& result) {
     result = value * 2;
     return true;
 }
 
 int main() {
-    // 1. 输出姓名和学号（请替换为你自己的真实信息）
-    std::cout << "姓名: 张三" << std::endl;
-    std::cout << "学号: 20230001" << std::endl;
 
-    // 2. 定义 bool 变量并输出
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+    std::cout << "姓名: 张辉" << std::endl;
+    std::cout << "学号: 2025010041" << std::endl;
+
     bool isStudent = true;
     std::cout << "是否为学生: " << isStudent << std::endl;
 
-    // 3. 用学号最后两位调用 getDoubled
-    int value = 1;       // 学号最后两位，请替换为你自己的
+    int value = 41;
     int result = 0;
     getDoubled(value, result);
     std::cout << value << " 的两倍是 " << result << std::endl;
